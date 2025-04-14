8f1be9eea344258a6e406de506f6d60408ae3e16
AFOEK <felix.montalfu@gmail.com>
2025-04-13 23:56:13 -0400

    Setting up alsa, fixng up terminal behavior, and fixing jump

--- main.asm
+++ main.asm
@@ -4,16 +4,8 @@ SECTION .note.GNU-stack noalloc noexec nowrite progbits
 SECTION .data   ;Declare variables
     Message: DB "Press (or hold) key to play note (A - K)", 10
     Message_len: EQU $-Message
-
-    Debug_Msg: DB "OK !", 10
-    Debug_Msg_len: EQU $-Debug_Msg
-
-    Message_err: DB "Error !",10
-    Message_err_len: EQU $-Message_err
-
-    Message_err_alsa: DB "Failed to initialized Alsa !", 10
-    Message_err_alsa_len: EQU $-Message_err_alsa
-
+    
+    ;Audio settings ALSA, and melody settings
     FreqTable:                      ;Declare frequency table
         DQ 2.0548023848490921e-1    ;C
         DQ 2.3064376937686967e-1    ;D
@@ -24,29 +16,47 @@ SECTION .data   ;Declare variables
         DQ 3.8789503773922857e-1    ;B
         DQ 4.1096047696981880e-1    ;C'
 
-    ;Audio_dev_path: DB "/dev/snd/pcmC0D0p",0    ;Sound device path
     Scale: DQ 2000  ;Scaling to convert to audible frequency
-    Sample_Rate: EQU 44100   ;Set sampling rate
+    Sample_Rate: DD 44100   ;Set sampling rate
     Segment_MS: EQU 40   ;Duration of each audio segment in ms (timing each note play, lower more smooth but more CPU)
     Amplitude: EQU 12000 ;16 bit signed amplitude (How loud the note Max: 32767)
-    Alsa_device: db "default", 0
+    Alsa_device: DB "plughw:0,0", 0
     SND_PCM_STREAM_PLAYBACK: EQU 1
     SND_PCM_FORMAT_S16_LE: EQU 2
     SND_PCM_ACCESS_RW_INTERLEAVED: EQU 3
 
-timespec:
-    tv_sec: DD 0    ; Seconds
-    tv_nsec: DD 10000000 ;Nano seconds (10 ms)
+    ;Terminal raw mode settings
+    TCGETS: EQU 21505
+    TCSETS: EQU 21506
+
+    timespec:
+        tv_sec: DD 0    ; Seconds
+        tv_nsec: DD 10000000 ;Nano seconds (10 ms)
+
+    ;Key map
+    Key_map: DB 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k'
+    Note_map: DB 0, 1, 2, 3, 4, 5, 6, 7
 
 SECTION .bss    ;Uninitialized data section / memory reserve
     Input_buff: RESB 1   ; Buffer for keyboard input
     Audio_fd: RESB 1 ;File descriptor for audio file
-    Phase_rad: RESB 1    ;Current phase increment (Rad/sample)
+    Phase_rad: RESQ 1    ;Current phase increment (Rad/sample)
     Last_key: RESB 1 ;Store last key value (0 if None)
-    Phase_acc: RESB 1    ;Phase accumulator (64 bit precision)
+    Phase_acc: RESQ 1    ;Phase accumulator (64 bit precision)
     Alsa_handle: RESD 1 ;Alsa handle param
     Alsa_params: RESD 1 ;Alsa param
     Alse_buffer: RESD 1 ;Alsa buffer
+    Samples_per_segment: EQU (44100 * Segment_MS / 1000)  ; = 1764 samples
+    Sample_buffer: RESW Samples_per_segment  ; 16-bit samples
+    Termios:
+        c_iflag RESD 1  ;Input mode flags
+        c_oflag RESD 1  ;Output mode flags
+        c_cflag RESD 1  ;Control mode flags
+        c_lflag RESD 1  ;Local mode flags (ICANON and ECHO)
+        c_line RESD 1   ;Line discipline
+        c_cc RESB 64    ;Control character
+
+    Old_termios: RESB 128   ;To store default termios settings
 
 
 SECTION .text   ;Code section
@@ -56,10 +66,12 @@ SECTION .text   ;Code section
     extern snd_pcm_hw_params_any, snd_pcm_hw_params_set_access
     extern snd_pcm_hw_params_set_format, snd_pcm_hw_params_set_channels
     extern snd_pcm_hw_params_set_rate_near, snd_pcm_hw_params
-    extern snd_pcm_writei, snd_pcm_prepare
+    extern snd_pcm_writei, snd_pcm_prepare, snd_pcm_hw_params_free
+    extern snd_strerror
     global main   ;Set starting point
 
 main:         ;main()
+    CALL Set_raw_mode   ;Set raw_mode
     MOV EAX, 4  ;Call sys_write
     MOV EBX, 1  ;stdout
     MOV ECX, Message    ;message content
@@ -72,15 +84,15 @@ main:         ;main()
     ; MOV EDX, 0  ; No mode (Permission control flag)
     ; INT 80H ;sys_call
 
-    CALL Init_alsa
-    CMP EAX, 0
-    JNE Error_exit
+    CALL Init_alsa  ;Setting up Alsa device
+    CMP EAX, 0      ;Compare if Alsa sub routine return 0 == success
+    JNE Error_exit  ;Jump if not or equal zero
 
-    CMP EAX, 0  ;Compare EAX (sys_open return value) == 0
-    JL exit ;Jump if EAX < 0 else continue
-    MOV [Audio_fd], EAX ; Set file descriptor memory value to EAX
+    MOV EAX, [Alsa_handle] ; Load ALSA handle
+    MOV [Audio_fd], EAX    ; Save it as file descriptor equivalent
+
     ;;Set non block stdin (default; stdin always terminate if )
-    MOV EAX, 55 ;Call sys_fcntl
+    MOV EAX, 55 ;sys_fcntl
     MOV EBX, 0  ;stdin
     MOV ECX, 3  ;F_GETFL (get current flag)
     INT 80H     ;sys_call
@@ -91,6 +103,31 @@ main:         ;main()
     MOV EDX, EAX    ;MOV EAX (New flag value {OE EAX, 2048} to EDX)
     MOV EAX, 55     ;sys_fnctl
     INT 80H         ;sys_call
+    JMP main_loop
+
+Set_raw_mode:
+    ;Switch stdin to raw input mode
+    MOV EAX, 54     ;sys_ioctl
+    MOV EBX, 0      ;stdin
+    MOV ECX, TCGETS ;Get terminal attributes
+    MOV EDX, Termios    ;Pointer to output termios struct
+    INT 80H             ;sys_call
+
+    MOV ESI, Termios    ;Source pointer to current settings
+    MOV EDI, Old_termios    ;Destination pointer to old settings
+    MOV ECX, 72             ;Number byte to copy
+    REP MOVSB               ;Copy termios struct byte by byte
+
+    ;-7 = 0xFFFFFFF9 = ~(1<<1 | 1<<2)
+    AND dword [c_lflag], -7 ;Disable line buffering and input echo (clear bits 1 (ICANON) and 2 (ECHO)
+    
+    ;ioctl(stdin, TCSETS, &Termios)
+    MOV EAX, 54     ;sys_ioctl
+    MOV EBX, 0      ;stdin
+    MOV ECX, TCSETS ;Set terminal attributes
+    MOV EDX, Termios    ;Pointer to modified settings
+    INT 80H             ;sys_call
+    RET ;Return to function
 
 main_loop:
     ;Check keyboard input if no continue
@@ -105,7 +142,7 @@ main_loop:
 Short_pause:
     ;Sleep threading for 10ms to reduce CPU usage when it's idle
     MOV EAX, 162    ;sys_sleep
-    MOV EBX, timespec  ;declared time struct (set how long the CPU will sleep) 
+    MOV EBX, timespec  ;Declared time struct (set how long the CPU will sleep) 
     MOV ECX, 0  ;Put flag if timespec is nullable
     INT 80H ;sys_call
 
@@ -123,15 +160,22 @@ Check_keyboard:
 
     CMP byte [Input_buff], 'q'  ;Check if input buffer == 'q'
     JE Close_and_exit   ;Jump if [Input_buff] == 'q'
-    ;Check key range A - K (C - C')
-    CMP byte [Input_buff], 'a'  ;Check if input buffer == 'a'
-    JB Invalid_key ;Jump if [Input_buffer] < 'a'
-    CMP byte [Input_buff], 'k'  ;Check if input buffer == 'k'
-    JA Invalid_key ;Jump if [Input_buffer] > 'k'
+    
+    MOV AL, [Input_buff]
+    MOV ESI, Key_map
+    MOV ECX, 8
 
-    MOV AL, [Input_buff]    ;Store all valid key to 16 bit register
-    MOV [Last_key], AL  ;Get the last pressed key, stored to [Last_key]
-    JMP End_check   ;Jump to End_check
+Find_key:
+    CMP AL, [ESI]
+    JE Key_found
+    INC ESI
+    LOOP Find_key
+
+    JMP Invalid_key
+
+Key_found:
+    MOV [Last_key], AL
+    JMP End_check
 
 Invalid_key:
     MOV byte [Last_key], 0  ;Treat it as a key release
@@ -142,16 +186,9 @@ End_check:
     RET ;Return to last jump
 
 ;Audio generation, from mathematically generated sine wave
-
 Play_note_segment:
     PUSHA   ;Push current registers value to stack
-
-    MOV EAX, Sample_Rate    ;Set Sample rate
-    MOV EBX, Segment_MS     ;Set segment timing
-    MUL EBX                 ;EAX = EAX * EBX
-    MOV EBX, 1000           ;Set EBX = 1000
-    DIV EBX                 ;EAX = EAX / EBX (1000)
-    MOV ECX, EAX            ;Set ECX = EAX
+    MOV ECX, Samples_per_segment ;Get pre-calculated sample per segment
 
     MOVZX EAX, byte [Last_key] ;Move byte to double word (last_key) with zero extention
     SUB EAX, 'a'        ;EAX = EAX - 'a'; Convert to 0 based index
@@ -168,23 +205,21 @@ Play_note_segment:
     PUSH EAX    ;Push EAX value to stack for FPU
     FIDIV dword [ESP]   ;ST0 = (2pi * frequency) / Sample_rate
     ADD ESP, 4  ;Clean up stack
-    FSTP dword [Phase_rad]  ;Store phase_rad in memory (pop ST0)
+    FSTP qword [Phase_rad]  ;Store phase_rad in memory (pop ST0)
     FLD qword [Phase_acc]   ;Load 64-bit phase accumulator
+    MOV EDI, Sample_buffer  ;EDI = start of sample buffer
 
 Generate_samples:
     FLD ST0     ;Duplicate current phase (ST0 -> ST1, ST0 = phase)
     FSIN        ;ST0 = sin(phase)
+    
     MOV EAX, Amplitude  ;Prepare amplitude for multipication
     PUSH EAX    ;Store on stack for FPU access
     FIMUL dword [ESP]   ;ST0 = sin(phase) * AMPLITUDE
     ADD ESP, 4  ;Clean up stack
-    FISTP word [Input_buff] ;Store sample in memory (input memory), pop ST0
     
-    MOV EAX, 4  ;sys_write
-    MOV EBX, [Audio_fd] ;Audio device file descriptor
-    MOV ECX, Input_buff ;Pointer to sample data
-    MOV EDX, 2  ;2 byte per sample (16 bit)
-    INT 80H ;sys_call
+    FISTP word [EDI]    ;Store 16-bit sample
+    ADD EDI, 2          ; Advance buffer pointer by 2
 
     FLD dword [Phase_rad]   ;Load phase increment (ST0)
     FADDP ST1, ST0          ;ST1 = ST1 + ST0, pop ST0 (Phase_rad += increment)
@@ -192,16 +227,27 @@ Generate_samples:
     FLDPI   ;Load pi to ST0
     FADD ST0, ST0   ;ST0 = 2pi
     FCOMIP ST1     ;Compare 2pi (ST0) with phase (ST1), pop ST0
+
     JBE Phase_wrap  ;Jump if ST0 <= ST1
     JMP Phase_ok    ;else continue
 
 Phase_wrap:
-    FSUB ST0, ST0   ;Phase -= 2pi
+    FLDPI   ;Load pi to ST0
+    FADD ST0, ST0   ;ST0 = 2pi
+    FSUB ST1, ST0   ;Phase -= 2pi
 
 Phase_ok:
     LOOP Generate_samples   ;Decrement ECX, jump if not zero
 
     FSTP qword [Phase_acc]  ;Store 64-bit phase accumulator, pop ST0
+
+    ; snd_pcm_writei(&handle, sample_buffer, sample_per_segment)
+    PUSH Samples_per_segment    ;Sample per segment
+    PUSH Sample_buffer          ;Sample buffer
+    PUSH dword [Alsa_handle]    ;Alsa handle
+    CALL snd_pcm_writei         ;Call ALSA write function
+    ADD ESP, 12                 ;Clear the stack (3 args * 4 byte)
+
     POPA    ;Restore all register value
     RET     ;Return from subroutine
 
@@ -209,7 +255,7 @@ Init_alsa:
     MOV dword [Alsa_handle], 0  ;Clear ALSA device
     MOV dword [Alsa_params], 0  ;Clear hardware parameters pointer
 
-    ; snd_pcm_open(&handle, "default", SND_PCM_STREAM_PLAYBACK, 0)
+    ; snd_pcm_open(&handle, device_name, SND_PCM_STREAM_PLAYBACK, 0)
     PUSH 0  ;Mode (0 default)
     PUSH SND_PCM_STREAM_PLAYBACK    ;Stream type (playback)
     PUSH Alsa_device    ;Device name
@@ -264,8 +310,8 @@ Init_alsa:
 
     ; snd_pcm_hw_params_set_rate_near(handle, params, &Sample_Rate, 0)
     PUSH 0      ;Direction (0 = exact/nearest)
-    LEA EAX, [Sample_Rate]
-    PUSH EAX    ;Sample rate (44100)
+    LEA EAX, [Sample_Rate]  ;Load the variable memory to EAX
+    PUSH EAX    ;Push memory address (Sample rate (44100)) to stack
     PUSH dword [Alsa_params]    ;Hardware params
     PUSH dword [Alsa_handle]    ;PCM handle
     CALL snd_pcm_hw_params_set_rate_near    ;Set sample rate
@@ -273,16 +319,43 @@ Init_alsa:
     TEST EAX, EAX       ;Test return value
     JNZ Error           ;Jump if error
 
+    ; snd_pcm_hw_params(handle, params)
+    PUSH dword [Alsa_params]    ;Push Alsa parameter
+    PUSH dword [Alsa_handle]    ;Push Alsa handle
+    CALL snd_pcm_hw_params      ;Set all previous config to the device
+    ADD ESP, 8                  ;Clean the stack
+    TEST EAX, EAX               ;Test return value
+    JNZ Error                   ;Jump if error
+
+    ; snd_pcm_prepare(handle)
     PUSH dword [Alsa_handle]    ;PCM handle
     CALL snd_pcm_prepare        ;Prepare device
     ADD ESP, 4                  ;Clean up stack
+    TEST EAX, EAX               ;Test return value
+    JNZ Error                   ;Jump if error
 
-    XOR EAX, EAX                ;Set return value to 0
     RET                         ;Return to function
 
 Error:
+    ;Push return value from ALSA
+    PUSH EAX
+    CALL Print_alsa_error
     ;Return -1
     MOV EAX, -1
+    RET
+
+Print_alsa_error:
+    MOV EBX, EAX    ; EBX = EAX (Copy error code from the ALSA)
+    PUSH EBX        ; Push error code to stack
+    ; snd_strerror(int errnum)
+    CALL snd_strerror   ;Get ALSA error string
+    ADD ESP, 4     ;Clean up stack
+
+    MOV ECX, EAX    ;Set buffer for the error message
+    MOV EAX, 4      ;sys_write
+    MOV EBX, 1      ;stdout
+    MOV EDX, 100    ;Set buffer size = 100
+    INT 80H         ;sys_call
     RET
 
 Alsa_fail:
@@ -291,32 +364,47 @@ Alsa_fail:
     PUSH dword [Alsa_handle]        ;PCM handle to close
     CALL snd_pcm_close              ;Close device
     ADD ESP, 4                      ;Clean up stack
+    MOV dword [Alsa_handle], 0      ;Set [Alsa_handle] = 0
     JMP Error_exit                  ;Return error
 
 Close_and_exit:
     ; MOV EAX, 6  ;sys_close
     ; MOV EBX, [Audio_fd] ;Audio file descriptor
     ; INT 80H ;sys_call
+    CMP dword [Alsa_handle], 0     ;Compare if [Alsa_handle] == 0
+    JE Ok_exit                     ;Jump if equal
     PUSH dword [Alsa_handle]       ;PCM handle to close
     CALL snd_pcm_close             ;Close device
     ADD ESP, 4                     ;Clean up stack
-    MOV EAX, 1                     ;sys_exit
-    XOR EBX, EBX                   ;Exit code 0
-    INT 80H                        ;system call
+    MOV dword [Alsa_handle], 0     ;Set [Alsa_handle] = 0
 
 Error_exit:
-    MOV EAX, 4
-    MOV EBX, 1
-    MOV ECX, Message_err_alsa
-    MOV EDX, Message_err_alsa_len
-    INT 80H
+    MOV EBX, 1  ;Set return 1
+    JMP exit    ;Jump to exit
 
-    MOV EAX, 1
-    MOV EBX, 1
-    ;XOR EBX, EBX
-    INT 80H
+Ok_exit:
+    XOR EBX, EBX    ;Set return 0
+    JMP exit        ;Jump to exit
+
+Restore_terminal:
+    MOV ESI, Old_termios    ;Source pointer to old settings
+    MOV EDI, Termios        ;Destination to current settings
+    MOV ECX, 72             ;Number byte to copy
+    REP MOVSB               ;Copy termios struct byte by byte
+
+    MOV EAX, 54             ;sys_ioctl
+    MOV EBX, 0              ;stdin
+    MOV ECX, TCSETS         ;Set terminal attributes
+    MOV EDX, Termios        ;Pointer to restored settings
+    INT 80H                 ;sys_call
+    RET                     ;Return to function
 
 exit:
+    ; snd_pcm_hw_params_free(&Alsa_params)
+    PUSH dword [Alsa_params]    ;Push alsa_params to stack
+    CALL snd_pcm_hw_params_free ;Free hardware parameters
+    ADD ESP, 4                  ;Clear stack
+
+    CALL Restore_terminal   ;Set terminal setting to default
     MOV EAX, 1  ;sys_exit
-    MOV EBX, 0  ;return 0;
     INT 80H ;sys_call
